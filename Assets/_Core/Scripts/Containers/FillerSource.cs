8261967b3d5382c5c1795f472524f7aa546c8c4d
Makar Dzezhamesau <dzezhamesau.work@gmail.com>
2022-10-07 17:30:15 +0300

    Added filler source

---
+++ Assets/_Core/Scripts/Containers/FillerSource.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace GrayCube.Containers
+{
+    public class FillerSource : MonoBehaviour
+    {
+        [SerializeField] private GameObject _fillerPrefab;
+
+        private IObjectPool<IFiller> _pool;
+        private IFiller _currentFiller;
+
+        private void Awake()
+        {
+            InitObjectPool();
+
+            ReleaseFiller();
+        }
+
+        private void InitObjectPool()
+        {
+            _pool = new ObjectPool<IFiller>(
+                CreateFiller,
+                (filler) => ((MonoBehaviour)filler).gameObject.SetActive(true),
+                (filler) => ((MonoBehaviour)filler).gameObject.SetActive(false)
+            );
+        }
+
+        private IFiller CreateFiller()
+        {
+            var filler = Instantiate(_fillerPrefab);
+            filler.transform.position = transform.position;
+            return filler.GetComponent<IFiller>();
+        }
+
+        private void ReleaseFiller()
+        {
+            var filler = _pool.Get();
+            filler.Filled += OnFillerFilled;
+
+            _currentFiller = filler;
+        }
+
+        private void OnFillerFilled()
+        {
+            _currentFiller.Filled -= OnFillerFilled;
+            ReleaseFiller();
+        }
+    }
+}
